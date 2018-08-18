713c2c0116c76b6b6bd1c31d2445d66239fd4d01
Rhonda Glennon <rhondaglennon@gmail.com>
2018-08-18 16:21:48 -0700

    add loops to build game boards

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,47 @@
-const printBoard = (board) => {
-    console.log("Current Board: ")
-    console.log(board[0].join(" | "));
-    console.log(board[1].join(" | "));
-    console.log(board[2].join(" | "));
+const generatePlayerBoard = (numberofRows, numberOfColumns) => {
+    const board = [];
+    for (let i = 0; i < numberofRows; i++) {
+        const row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(" ");
+
+        }
+        board.push(row);
+    }
+    return board;
+};
+
+const generateBombBoard = (numberofRows, numberOfColumns, numberOfBombs) => {
+    const board = [];
+    for (let i = 0; i < numberofRows; i++) {
+        const row = [];
+        for (let j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+
+        }
+        board.push(row);
+    }
+
+    let numberOfBombsPlaced = 0;
+    // TODO: do not place bombs on top of existing bombs
+    while (numberOfBombsPlaced < numberOfBombs) {
+        const randomRowIndex = Math.floor(Math.random() * numberofRows);
+        const randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = "B";
+        numberOfBombsPlaced++;
+    }
+    return board;
 }
 
-let board = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
-];
+const printBoard = (board) => {
+    console.log(board.map(row => row.join(" | ")).join("\n"));
+}
 
-printBoard(board);
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
 
-board[0][1] = "1";
-board[2][2] = "B";
+console.log("Player Board: ");
+printBoard(playerBoard);
 
-printBoard(board);
+console.log("Bomb Board: ");
+printBoard(bombBoard);
